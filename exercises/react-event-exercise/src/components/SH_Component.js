52e91175333bdb0ea0fbbf6997ee9ae4e82f4c30
Mak Karmarkar <mkarmarkar@gmail.com>
2017-10-09 18:37:43 -0600

    superhero list that has event handlers

--- exercises/react-event-exercise/src/components/SH_Component.js
+++ exercises/react-event-exercise/src/components/SH_Component.js
@@ -1,16 +1,17 @@
 import React from "react";
-import index from "../index.css";
 
 function SH_Component(props){
 
   function handleClick(){
     alert(props.hero.catchPhrase)
   }
+
   return(
     <div className="SH_box">
-      <h2 onClick={()=>handleClick()}> Superhero Name: {props.hero.name}</h2>
+      <h3> Superhero Name: </h3>
+      <h3 onClick={()=>handleClick()} style={{fontFamily: "Helvetica", color: "chocolate"}}> {props.hero.name}</h3>
       <img className="photo" onClick={()=>handleClick()}src={props.hero.image}
-      key={props.hero.name}/>
+      key={props.hero.name} alt={prop.hero.name}/>
     </div>
   )
 }
