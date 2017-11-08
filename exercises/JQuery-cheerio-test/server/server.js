47d41cc8bf76fe2f368430f49ff39f678fca8a8f
Mak Karmarkar <mkarmarkar@gmail.com>
2017-11-07 23:56:45 -0700

    close to final assignment changes

--- exercises/JQuery-cheerio-test/server/server.js
+++ exercises/JQuery-cheerio-test/server/server.js
@@ -1,51 +1,51 @@
-const express = require("express");
-const request = require("request");
-const fs = require("fs");
-const cheerio = require("cheerio");
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-
-app.get('/scrape', (req, res)=>{
-  url = 'http://www.imdb.com/title/tt1229340/';
-
-  request(url, (error, response, html)=>{
-    if(!error){
-      const $ = cheerio.load(html);
-      let title, release, rating;
-      let json = {title: "", release: "", rating: ""};
-
-      $('.header').filter(()=>{
-        let data = $(this);
-        title = data.children().first().text();
-        release = data.children().last().children().text();
-        json.title = title;
-        json.release = release;
-        console.log(title);
-        console.log(release);
-      })
-
-      $('.star-box-giga-star').filter(()=>{
-               let data = $(this);
-               rating = data.text();
-                json.rating = rating;
-                console.log(rating);
-              })
-
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), (err)=>{
-
-      console.log('File successfully written! - Check your project directory for the output.json file');
-})
-}
-        res.send('Check your console!')
-
-      });
-})
-
-
-
-app.listen(PORT, ()=>{
-  console.log("server is running on port " + PORT);
-})
-
-exports = module.exports = app;
+// const express = require("express");
+// const request = require("request");
+// const fs = require("fs");
+// const cheerio = require("cheerio");
+// const app = express();
+//
+// const PORT = process.env.PORT || 4000;
+//
+// app.get('/scrape', (req, res)=>{
+//   url = 'http://www.imdb.com/title/tt1229340/';
+//
+//   request(url, (error, response, html)=>{
+//     if(!error){
+//       const $ = cheerio.load(html);
+//       let title, release, rating;
+//       let json = {title: "", release: "", rating: ""};
+//
+//       $('.header').filter(()=>{
+//         let data = $(this);
+//         title = data.children().first().text();
+//         release = data.children().last().children().text();
+//         json.title = title;
+//         json.release = release;
+//         console.log(title);
+//         console.log(release);
+//       })
+//
+//       $('.star-box-giga-star').filter(()=>{
+//                let data = $(this);
+//                rating = data.text();
+//                 json.rating = rating;
+//                 console.log(rating);
+//               })
+//
+//     fs.writeFile('output.json', JSON.stringify(json, null, 4), (err)=>{
+//
+//       console.log('File successfully written! - Check your project directory for the output.json file');
+// })
+// }
+//         res.send('Check your console!')
+//
+//       });
+// })
+//
+//
+//
+// app.listen(PORT, ()=>{
+//   console.log("server is running on port " + PORT);
+// })
+//
+// exports = module.exports = app;
--- exercises/JQuery-cheerio-test/server/server2.js
+++ exercises/JQuery-cheerio-test/server/server2.js
@@ -2,32 +2,22 @@ const express = require("express");
 const request = require("request");
 const fs = require("fs");
 const cheerio = require("cheerio");
+const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const url = "https://www.smithsfoodanddrug.com/search?query=cheese&page=2&tab=0"
-// var items = [];
+const url = 'https://www.smithsfoodanddrug.com/search?query=cheese&page=2&tab=0'
+
+app.get('/scrape', (req, res)=>{
 request(url, (err, res, body)=>{
-  if(err){
-    let $ = cheerio.load(body);
-    $(".ProductCard-name").each((index, title)=>{
-      console.log($(title).text());
-    });
-  }
-
-  // if(!err){
-  //   var $ = cheerio.load(body);
-  //   $(".ProductCard-name").each(()=>{
-  //     var item = $(this).text();
-  //     items.push(item);
-    // });
-    // console.log(items);
-  // }
-
+  if(err) res.send(err)
+  console.log(body);
+});
+res.send('Check your console!')
 });
 
-// app.listen(PORT, ()=>{
-//   console.log("server is running on port " + PORT);
-// })
-//
-// exports = module.exports = app;
+app.listen(PORT, ()=>{
+  console.log("server is spinning on port " + PORT);
+})
+
+exports = module.exports = app;
