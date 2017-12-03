f0ff3c82061f5faebede71b14d0676dac695f933
Mak Karmarkar <mkarmarkar@gmail.com>
2017-12-02 23:27:50 -0700

    personal profile page

--- personal-profile/client/src/main/App.js
+++ personal-profile/client/src/main/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Switch, Route} from "react-router-dom";
+
 
 import Header from "./views/Header";
 import Body from "./views/Body";
 import Footer from "./views/Footer";
 import LandPage from "./views/LandPage";
+import Profile from "./view/Resume";
 
 function App(){
   render(
@@ -11,6 +14,7 @@ function App(){
         <LandPage />
         <Header />
         <Body />
+        <Resume />
         <Footer />
     </div>
   )
---
+++ personal-profile/client/src/main/views/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <div className="footer-wrap">
+      <div className="headText">Skills: </div>
+      <div>
+          <span className="a">
+              <a
+                  target="_blank"
+                  className="links"
+                  href="https://github.com/jamie29w">
+                  <i className="fa fa-github fa-lg" aria-hidden="true" />
+              </a>
+          </span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
