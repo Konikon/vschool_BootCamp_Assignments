ed510fbe161e6ae9ecb737343df6989abf0a9761
Mak Karmarkar <mkarmarkar@gmail.com>
2017-12-20 15:09:46 -0700

    links to header and footer

--- personal-profile/client/src/main/views/Header.js
+++ personal-profile/client/src/main/views/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header(){
     return(
         <div>
-        Hi header!
+        <Link to="/"> Home </Link>
+        <Link to="/linkedin">Projects</Link>
+        <Link to="/github"> github </Link>
         </div>
     )
 }
