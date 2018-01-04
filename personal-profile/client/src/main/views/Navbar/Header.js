e46a516bff18e3d2e10ba1bbfab55589342cca63
Mak Karmarkar <mkarmarkar@gmail.com>
2018-01-03 23:46:49 -0700

    logo

--- personal-profile/client/src/main/views/Navbar/Header.js
+++ personal-profile/client/src/main/views/Navbar/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import linkedin from "../../images/linkedin.jpg";
+import github3 from "../../images/github3.png"
+
+const Linkedin = "https://www.linkedin.com/in/mkarmarkar/";
+const Github = "https://github.com/Konikon"
 
 function Header(){
     return(
-        <div className="header_wrap">
-        <Link to="/"> mak </Link>
-        <Link to="/Projects">Projects</Link>
-        <Link to="/Resume"> Resume </Link>
-        <a> </a>
-        </div>
+      <div>
+          <div className="header_wrap">
+              <Link to="/"> mak </Link>
+              <Link to="/Projects">Projects</Link>
+              <Link to="/Resume"> Resume </Link>
+
+            <a href="Linkedin"><img src={linkedin} /> </a>
+            <a href="Github"> <img src={github3} /> </a>
+          </div>
+      </div>
     )
 }
 
