73d683a7f7b95dcb49d89975b6cc90b521c75e6c
Mak Karmarkar <mkarmarkar@gmail.com>
2018-01-28 23:52:56 -0700

    landing page filpper centering

--- personal-profile/client/src/main/views/LandPage.js
+++ personal-profile/client/src/main/views/LandPage.js
@@ -1,20 +1,26 @@
 import React from "react";
 import Ms from "../images/Msaffron.png"
+import Ms2 from "../images/Msaffron2.png"
 import {Link} from "react-router-dom";
 
 function LandPage(){
     return(
         <div className="landPage_wrapper">
-          <span className="landPage_name">M<span >akarand</span> A K<span >armarkar </span></span>
-          <hr/>
-            <div className="landPage_name2">
-                <Link to="/projects"> Projects</Link>
-                <Link to="/resume" > Resume </Link>
+          <div>
+            <span className="landPage_name">M<span >akarand</span> A K<span >armarkar </span></span>
+              <div className="landPage_name2">
+                  <Link to="/projects"> Projects</Link>
+                  <Link to="/resume" > Resume </Link>
+              </div>
+              <div className="flipper">
+                    <div className="front card">
+                        <img src={Ms} alt="logo" style={{background: "center"}}></img>
+                    </div>
+                    <div className="back card">
+                        <img src={Ms2} alt="logo" className="center"></img>
+                    </div>
+              </div>
             </div>
-            <div>
-            <img src={Ms} alt="logo"></img>
-            </div>
-            <hr/>
         </div>
     )
 }
